57ce2c8c9eacff7ffaaf4952d3d610841b09eda6
VAIBHAV TYAGI <arjun0503tyagi@gmail.com>
2024-07-09 17:36:11 +0530

    save for try

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const mongoUrl ="mongodb://localhost:27017/TyagiMotorReg"
 require('dotenv').config();
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = process.env.MONGODB_URL;
 
 mongoose.connect(mongoUrl,{
     // useNewUrlParser: true,
